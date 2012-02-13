c1599dc630b10b8125502abadd853e438e538e83
Jonathan Kim <jkimbo@gmail.com>
2012-02-13 18:16:59 +0000

    Added location and marker to map

--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,51 @@ $(function() {
     $('#map_canvas').width($(window).width());
     $('#map_canvas').height($(window).height());
     var options = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+        center: new google.maps.LatLng(51.50812890, -0.1280050),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     var map = new google.maps.Map(
             document.getElementById("map_canvas"),
             options
     );
+
+    var position;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var current = new google.maps.LatLng(
+                position.coords.latitude, 
+                position.coords.longitude);
+            map.setCenter(current);
+            var position = new google.maps.Marker({
+                position: current,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title:"There you are!"
+            });
+            google.maps.event.addListener(position, 'click', toggleBounce);
+            function toggleBounce() {
+                if (position.getAnimation() != null) {
+                    position.setAnimation(null);
+                } else {
+                    position.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function() {
+                        position.setAnimation(null);
+                    }, 1000);
+                }
+            }
+        }, function(msg) {
+            alert('error: '+msg);
+        });
+    } else {
+          alert('geolocation not supported');
+    }
+
+    $(window).resize(function() {
+        $('#map_canvas').width($(window).width());
+        $('#map_canvas').height($(window).height());
+    });
 });
+
